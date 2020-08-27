7274b1cea84bf4b84c3546ef44952cff667ba528
irina <irina.kazazyan@servicebox.ge>
2020-08-27 17:56:52 +0400

    added database and brew_list

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,25 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/home/brew_list.dart';
 import 'package:flutter_firebase/services/auth.dart';
+import 'package:flutter_firebase/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    final AuthService _auth = AuthService();
-
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Flutter firebase'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('logout'))
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Flutter firebase'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('logout'))
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
